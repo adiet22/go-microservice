ae1d4fd7505ab94706b5e1fa7754384a10179d27
Adiet Alimudin <55053994+adiet95@users.noreply.github.com>
2022-11-06 12:54:30 +0700

    add broker post

---
+++ broker-service/src/api/routes.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+)
+
+func (app *Config) routes() http.Handler {
+	chi := chi.NewRouter()
+
+	chi.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	chi.Use(middleware.Heartbeat("/ping"))
+	chi.Post("/", app.Broker)
+
+	return chi
+}
